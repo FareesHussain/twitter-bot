6ce807b631aecc7cc448134e76e2439c7b01641a
Farees Hussain <fareezzhussain@gmail.com>
2020-09-29 14:58:30 +0530

    retweet the tracked tweets

--- index.js
+++ index.js
@@ -14,15 +14,46 @@ T.get('account/verify_credentials',{
     include_email: false
 }, onAuthenticated)
 
+const handle = "sarcasticdevel1"
+
+const termsToTrack = [
+    "#Kotlin",
+    "#AndroidDevs",
+    "#NowInAndroid",
+    "#AndroidNewbie",
+    "#thisIsNothingButATestTweet",
+    "#KotlinTips"
+]
+
 function onAuthenticated(err){
-    if(err){
-        console.log("an Error occured: "+err)
-    } else {
-        console.log('Authenticated successfully')
-        sendTweet()
+    if(err) console.log("An Error occured: "+err)
+    else {
+        console.log('Authenticated Successfully')
+        // sendTweet() // unComment it for testing purposes
+        trackTweets()
     }
 }
 
+function trackTweets(){
+    var stream = T.stream('statuses/filter', {track:termsToTrack, tweet_mode:'extended'})
+
+    stream.on('tweet', function (tweet) {
+        if(!tweet.retweeted_status && tweet.user.screen_name !== handle){
+            console.log(tweet.text)
+            console.log(tweet.id_str + tweet.name)
+            makeRetweet(tweet.id_str)
+            //todo add a new function to like the tweets
+        }
+    })
+}
+
+function makeRetweet(tweetId) {
+    T.post('statuses/retweet', { id: tweetId })
+        .then(result => {
+            console.log('Retweeted successfully!');
+        }).catch(console.error);
+}
+
 function sendTweet() {
-    T.post('statuses/update',{ status : 'this is my First Tweet from the bot #EnjoyYourDay' })
+    T.post('statuses/update',{ status : 'test tweet 8 to retrack tweet bases on #thisIsNothingButATestTweet' })
 }
